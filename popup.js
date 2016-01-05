6b7e518091977fad1f39befc949ba718d3a5f354
lylynguyen <lylynguyen812@gmail.com>
2016-01-05 01:12:02 -0800

    fixed content scripts

--- popup.js
+++ popup.js
@@ -1,9 +1,12 @@
-
-
 var wishListApp = angular.module('wishListApp', [])
 wishListApp.factory('wishListFactory', function(){
   return {
-    url: window.location.href
+    // url: window.location.href
+    chrome: function(){
+      chrome.tabs.getSelected(null, function(tab) {
+        current_tab = tab;
+      }
+    }
   }
 });
 
@@ -11,3 +14,13 @@ wishListApp.controller('wishListCtrl', function($scope, wishListFactory){
   $scope.url = wishListFactory.url;
 
 });
+
+
+// function init()
+// {
+//   chrome.tabs.getSelected(null, function(tab) 
+//   {
+//     current_tab = tab;
+//     background_page.shortenUrl(tab.url, tab.incognito, onResponse);
+//   });
+// }
