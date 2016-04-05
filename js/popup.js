0066f7958f1b1fe30a2f0aa1b58c98bfc3c752f1
lylynguyen <lylynguyen812@gmail.com>
2016-04-05 01:01:02 -0700

    added search function

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,10 @@
 var wishListApp = angular.module('wishListApp', [])
 
+
 wishListApp.service('wishListService', function () {
   this.getUrl = function(callback) {
     var model = {};
-// Gets all tabs that have the specified properties, or all tabs if no properties are specified.
+
     chrome.tabs.query({'active': true},
     function (tabs) {
       if (tabs.length > 0)
@@ -11,8 +12,6 @@ wishListApp.service('wishListService', function () {
         model.title = tabs[0].title;
         model.url = tabs[0].url;
 
-// Sends a single message to the content script(s) in 
-// the specified tab, with an optional callback to run when a response is sent back. 
         chrome.tabs.sendMessage(tabs[0].id, { 'action': 'GetUrl' }, function (response) {
           model.getUrls = response;
           callback(model);
@@ -23,16 +22,16 @@ wishListApp.service('wishListService', function () {
 });
 
 wishListApp.controller('wishListCtrl', function($scope, wishListService){
-    $scope.amount =0;
+  $scope.amount =0;
 
-    $scope.getUrl = function () {
-      wishListService.getUrl(function(info){
-    $scope.productName = info.title;
-    $scope.url = info.url;
-    $scope.getUrls = info.getUrls;
-    $scope.$apply();
+  $scope.getUrl = function () {
+    wishListService.getUrl(function(info){
+  $scope.productName = info.title;
+  $scope.url = info.url;
+  $scope.getUrls = info.getUrls;
+  $scope.$apply();
   });
-    }
+  }
 
 
 
@@ -42,7 +41,7 @@ wishListApp.controller('wishListCtrl', function($scope, wishListService){
       wishList.push({"Product": $scope.productName, "URL": $scope.url, "Price": $scope.amount})
       chrome.storage.local.set({wishList: wishList}, function(){
         chrome.storage.local.get('wishList', function(result){
-          $scope.test = result.wishList.reverse();
+          $scope.list = result.wishList.reverse();
           // console.log($scope.test);
         });
       });
@@ -50,16 +49,17 @@ wishListApp.controller('wishListCtrl', function($scope, wishListService){
   };
 
   $scope.delete = function () {
-    chrome.storage.local.get({wishList: []}, function (item) {
-      item.wishList.splice(0,1);
-      chrome.storage.local.set(item, function () {
-        console.log('deleted!');
-      })
-    })
+    chrome.local.storage.remove(item);
+    // chrome.storage.local.get({wishList: []}, function (item) {
+    //   item.wishList.splice(0,1);
+    //   chrome.storage.local.set(item, function () {
+    //     console.log('deleted!');
+    //   })
+    // })
   };
 });
 
-// chrome.storage.local.clear()
+
 
 
 
